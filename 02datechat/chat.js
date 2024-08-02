fe307a714df3ba2a29555a2564c5926e6ff6c0e3
ishit10 <kapuparaishit18@gmail.com>
2024-08-02 10:19:46 +0530

    done

---
+++ 02datechat/chat.js
@@ -0,0 +1,89 @@
+console.log("hello");
+
+let virat = prompt();
+
+if (virat=="") {
+    alert("typing.....");
+    
+    
+}
+else{
+    alert("Hi!");
+
+    let rohit = prompt();
+    if (rohit =="") {
+        alert("typing.....");
+    } else {
+        alert("My favorite player is Virat Kohli. What about you?")
+
+        let rohit = prompt();
+    if (rohit =="") {
+        alert("typing.....");
+    }
+    else{
+        alert("Absolutely! Dhoni's calmness under pressure is unmatched.")
+
+        let rohit = prompt();
+        if (rohit =="") {
+            alert("typing.....");
+        } else {
+            alert("Yes, that was an incredible match. Kohli also played a pivotal role in that game.")
+
+
+            let rohit = prompt();
+            if (rohit =="") {
+                alert("typing.....");
+            } else {
+                alert("Indeed. Indian cricket has a bright future with such talents.")
+
+                let rohit = prompt();
+            if (rohit =="") {
+                alert("typing.....");
+            } else {
+                alert("Yes, I am. The competition is intense this time!")
+
+                let rohit = prompt();
+                if (rohit =="") {
+                    alert("typing.....");
+                } else {
+                    alert("Fingers crossed! Let's catch up after the match to discuss the highlights.")
+
+                    let rohit = prompt();
+                if (rohit =="") {
+                    alert("typing.....");
+                } else {
+                    alert("Bye!")
+                }
+
+
+                }
+
+            }
+            }
+        }
+
+    }
+
+    }
+
+
+}
+
+
+
+
+
+// [Person 1] Hey, who is your favorite cricket player?
+// [Person 2] Hi! My favorite player is Virat Kohli. What about you?
+// [Person 1] That's a great choice! I really like MS Dhoni. 
+// [Person 2] Absolutely! Dhoni's calmness under pressure is unmatched.
+// [Person 1] Do you remember the 2011 World Cup final? His innings were crucial.
+// [Person 2] Yes, that was an incredible match. Kohli also played a pivotal role in that game.
+// [Person 1] True. It's great to see young players like Rishabh Pant coming up too.
+// [Person 2] Indeed. Indian cricket has a bright future with such talents.
+// [Person 1] By the way, are you following the current series?
+// [Person 2] Yes, I am. The competition is intense this time!
+// [Person 1] I can't wait for the next match. Hopefully, our team continues to perform well.
+// [Person 2] Fingers crossed! Let's catch up after the match to discuss the highlights.
+// [Person 1] Sounds good. Talk to you later!
+// [Person 2] Bye!
